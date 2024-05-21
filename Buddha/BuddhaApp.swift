79dbfe87067575f1043cc1056d1eecc4a038c7ca
Alex <alex@zoneonezone.com>
2024-05-21 09:56:54 +0700

    Initial Commit

---
+++ Buddha/BuddhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  BuddhaApp.swift
+//  Buddha
+//
+//  Created by Phung Chinh on 21/5/24.
+//
+
+import SwiftUI
+
+@main
+struct BuddhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Buddha/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Buddha
+//
+//  Created by Phung Chinh on 21/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
